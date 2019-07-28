25019f029271b7c13c53c78dec82e66b54289c39
AndreiD <davidandrei96@yahoo.com>
2019-07-28 18:38:14 +0300

    added security: login, register, webtokens

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Your password must be at lest 4 characters long!")]
+        public string Password { get; set; }
+    }
+}
